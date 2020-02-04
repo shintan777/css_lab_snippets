b9a7547a4ff79c6e5417bf612c3a02e73d6b0675
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-02-04 16:18:37 +0530

    Adds iverse subrotuone

--- Asymmetric Key Ciphers/rsa_basic.py
+++ Asymmetric Key Ciphers/rsa_basic.py
@@ -1,6 +1,16 @@
 """
 Basic rsa algorithm with encryption and decryption
 """
+
+def inverse(p,n):
+    """
+    Returns modular inverse of p in mod n
+    """
+    for i in range(2,p):
+        if (p*i)%n == 1:
+            return i
+    raise ValueError("No inverse exists")
+
 opt = input("Enter 1 for ecryption and 2 for decrytion\n")
 if opt=='1':
     s = input("Enter text\n")
@@ -19,7 +29,7 @@ else:
     k = input("Enter key or leave blank for bruteforce\n")
     if k != '':
         k = int(k)
-    if k != '': 
+    if k != '':
         c = []
         ##code here
         print("Plain text", ''.join(c))
@@ -27,6 +37,5 @@ else:
         print("Bruteforcing....")
         for k in range(1,26):
             c = []
-            ##code here    
+            ##code here
         print(" For k = ",k,"Plain text = ", ''.join(c))
-### Wrote on python idle ... indent is 1 tab 
