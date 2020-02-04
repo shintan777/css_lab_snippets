dd67cb64836649bec8a7a9a61ca0af488fc88dd4
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-02-04 15:58:54 +0530

    Adds basic template for rsa

--- Asymmetric Key Ciphers/rsa_basic.py
+++ Asymmetric Key Ciphers/rsa_basic.py
@@ -1,3 +1,32 @@
 """
 Basic rsa algorithm with encryption and decryption
 """
+opt = input("Enter 1 for ecryption and 2 for decrytion\n")
+if opt=='1':
+    s = input("Enter text\n")
+    s = s.lower()
+    s = s.split()
+    s = ''.join(s)
+    k = int(input("Enter key\n"))
+    c = []
+    """
+    ## code here
+    """
+    print("Cipher text", ''.join(c))
+else:
+    s = input("Enter Chipher text\n")
+    s = s.lower()
+    k = input("Enter key or leave blank for bruteforce\n")
+    if k != '':
+        k = int(k)
+    if k != '': 
+        c = []
+        ##code here
+        print("Plain text", ''.join(c))
+    else:
+        print("Bruteforcing....")
+        for k in range(1,26):
+            c = []
+            ##code here    
+        print(" For k = ",k,"Plain text = ", ''.join(c))
+### Wrote on python idle ... indent is 1 tab 
